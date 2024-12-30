165234e3d5561a0a76677edc62f38cd71feb6711
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2024-12-30 18:05:21 +0530

    Dynamic variables

--- 05-Dynamic-variables.sh
+++ 05-Dynamic-variables.sh
@@ -2,6 +2,6 @@
 
 echo "Please enter your name" # Text entered here will be added as value to the variable
 
-read USERNAME # Here USERNAME is a variable name
+read -s USERNAME # Here USERNAME is a variable name
 
 echo "User name entered is $USERNAME"
