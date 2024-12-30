45dcd396d72db3652ff11587185771a0fc1415ce
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2024-12-30 17:33:51 +0530

    Adding variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Dakshina #No space before and after =
+PERSON2=Ramesh
+
+echo "$PERSON1:: Hi, $PERSON2"
+echo "PERSON2:: Hell, $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am fine, How are you?"
