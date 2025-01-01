78b7ec17d81cfc294cb08c9d89444588b7c26118
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-01 22:14:07 +0530

    installation Script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,24 +8,37 @@ USERID=$(id -u)
             exit 1 #other than 0
     fi
 
+        dnf list installed mysql
+
+    if [ $? -ne 0 ] # not installed
+    then
         dnf install mysql -y
 
-    if [ $? -ne 0 ]
-    then
-            echo "Installation of mysql.. FAILURE"
-            exit 1
-    else
-            echo "Installation of mysql..SUCCESS"
+        if [ $? -ne 0 ]
+            then
+                echo "Installation of mysql.. FAILURE"
+                exit 1
+            else
+                echo "Installation of mysql..SUCCESS"
+            fi
+        else
+            echo "Mysql is already ....INSTALLED"
 
     fi
-
+            dnf list installed git
+        if [ $? -ne 0 ] # not installed
+    then
         dnf install git -y
 
-    if [ $? -ne 0 ]
+        if [ $? -ne 0 ]
     then
             echo "Installation of git.. FAILURE"
         exit 1
     else
         echo "Installation of git..SUCCESS"
+
+        fi
+     else
+        echo "GIT is already ....INSTALLED"
 
     fi
