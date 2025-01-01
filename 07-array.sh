9cfd6f5ffd140d69a68c40a6501a8f3f9b62ba0b
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-01 19:32:50 +0530

    array script

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("PUSHPA" "SRIMANTHUDU" "DEVARA")
+#index starts here from 0, size is 3
+
+echo "First movie name:$MOVIES[0]"
+echo "Second movie name:$MOVIES[1]"
+echo "Third movie name:$MOVIES[2]"
+
+echo "All movies are:$MOVIES[@]"
