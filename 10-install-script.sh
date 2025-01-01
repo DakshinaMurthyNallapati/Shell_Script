83bb5443295859d4b0ac10233deef34a881fa9e6
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-01 21:49:51 +0530

    installation Script

---
+++ 10-install-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+    if [ $USERID -ne 0 ]
+        then 
+            echo "Error:: You need sudo access to execute this script"
+            exit 1 #other than 0
+    fi
+
+        dnf install mysqll -y
+
+    if [ $? -ne 0 ]
+
+        echo "Installing mysql.. FAILURE"
+        exit 1
+    else
+        echo "Installing mysql..SUCCESS"
+
+    fi
+
+        dnf install git -y
+
+   if [ $? -ne 0 ]
+
+        echo "Installing git.. FAILURE"
+        exit 1
+    else
+        echo "Installing git..SUCCESS"
+
+    fi
