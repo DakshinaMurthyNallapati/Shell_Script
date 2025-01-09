23155acebdd61a3b4dd01a4fbfb98faf621097c5
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-09 19:47:17 +0530

    Old files to be deleted script

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SOURCE_DIR="/home/ec2-user/app-logs"
+#SOURCE_DIR="/home/ec2-user/app-logs"
 
 #LOGS_FOLDER="/home/ec2-user/app-logs"
 #LOG_FILE=$(echo $0 | cut -d "." -f1)
 #TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 #LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+FILES_TO_DELETE= $(find . -name "*.log" -mtime +14)
 echo "Files to be deleted: $FIlES_TO_DELETE"
 
