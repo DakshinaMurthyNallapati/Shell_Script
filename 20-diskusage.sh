82f3e846a1c38e1f103024fb6b7f26e8747c5063
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-13 23:07:50 +0530

    Disk Usage script

--- 20-diskusage.sh
+++ 20-diskusage.sh
@@ -12,7 +12,7 @@ while read -r line
 
         if [ $USAGE -ge $DISK_THRESHOLD ]
             then
-                MSG="High disk usage on partition : $PARTITION Usage is : $USAGE"
+                MSG+="High disk usage on partition : $PARTITION Usage is : $USAGE"
         fi
 
     done <<< $DISK_USAGE
