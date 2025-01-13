297e53ec3f4241e6b8d735dccae4d0d04fd293ee
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-13 23:05:43 +0530

    Disk Usage script

--- 20-diskusage.sh
+++ 20-diskusage.sh
@@ -2,11 +2,18 @@
 
 DISK_USAGE=$(df -HT | grep xfs)
 DISK_THRESHOLD=5 #In real projects threshold value will be 70
+MSG=""
 
 while read -r line
     do 
         USAGE=$(echo $line | awk -F " " '{print $6F}'| cut -d "%" -f1)
         PARTITION=$(echo $line | awk -F " " '{print $NF}')
-        echo "Partition: $PARTITION    Usage: $USAGE"
+        #echo "Partition: $PARTITION    Usage: $USAGE"
+
+        if [ $USAGE -ge $DISK_THRESHOLD ]
+            then
+                MSG="High disk usage on partition : $PARTITION Usage is : $USAGE"
+        fi
 
     done <<< $DISK_USAGE
+    echo "Message : $MSG"
