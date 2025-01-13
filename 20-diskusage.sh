482ee104dccf811d71127853580970c61830a703
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-13 22:43:57 +0530

    Disk Usage script

--- 20-diskusage.sh
+++ 20-diskusage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -HT | grep xfs)
+DISK_THRESHOLD=5 #In real projects threshold value will be 70
 
 while read -r line
     do 
-        echo $line
+        USAGE=$(echo $line | awk -F " " '{print $6F}')
+        PARTITION=$(echo $line | awk -F " " '{print $nF}')
+
     done <<< $DISK_USAGE
