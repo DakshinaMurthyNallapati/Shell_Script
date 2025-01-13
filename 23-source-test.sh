e2086bc6e2bc1259342f1bf9e24c53d29cedcb4a
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-14 00:07:39 +0530

    Disk Usage script

---
+++ 23-source-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ./common.sh
+SOURCE_DIR="/home/ec2-user/app_log"
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files to be deleted: $FILES_TO_DELETE"
+
+while read -r file
+    do
+        echo "Deleting file :$file"
+        rm -rf $file
+        echo "Files deleted successfully"
+
+    done <<< $FILES_TO_DELETE
