62ee0f12b94faf1d5e274075addb641dd3b7bd4b
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-01-13 22:57:38 +0530

    Disk Usage script

--- 20-diskusage.sh
+++ 20-diskusage.sh
@@ -6,7 +6,7 @@ DISK_THRESHOLD=5 #In real projects threshold value will be 70
 while read -r line
     do 
         USAGE=$(echo $line | awk -F " " '{print $6F}')
-        PARTITION=$(echo $line | awk -F " " '{print $nF}')
+        PARTITION=$(echo $line | awk -F " " '{print $NF}')
         echo "Partition : $PARTITION, Usage : $USAGE"
 
     done <<< $DISK_USAGE
